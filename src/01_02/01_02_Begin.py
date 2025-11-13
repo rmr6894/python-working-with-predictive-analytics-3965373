392170e885e7b0d1b070c84733f6a20bb6b1a58b
Ryan Rieger <36425617+rmr6894@users.noreply.github.com>
2025-11-13 09:21:53 +0000

    01_02

--- src/01_02/01_02_Begin.py
+++ src/01_02/01_02_Begin.py
@@ -1,6 +1,12 @@
 # Load necessary libraries
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
+import matplotlib.pyplot as plt
 
 # Load the dataset
+data = pd.read_csv("input/insurance.csv")
 
 # Display the first 15 rows of the dataset
-
+print(data.head(15))
