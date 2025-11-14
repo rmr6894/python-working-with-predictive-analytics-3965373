19ce8db990ccb185ac08d7dc45780bc71c00c656
Ryan Rieger <36425617+rmr6894@users.noreply.github.com>
2025-11-14 07:54:50 +0000

    01_06

--- src/01_06/01_06_Begin.py
+++ src/01_06/01_06_Begin.py
@@ -20,10 +20,32 @@ print(data.isnull().sum())
 
 # Label Encoding: Encode 'sex' and 'smoker' columns
 # TODO: Create a label encoder instance and encode the 'sex' column
+le = LabelEncoder()
+data["sex"] = le.fit_transform(data["sex"])
+print("\nSklearn Label Encoding for 'sex':")
+print(dict(zip(le.classes_, le.transform(le.classes_))))
+print(data[["sex"]].head(10))
 
 # TODO: Create a label encoder instance and encode the 'smoker' column
+data["smoker"] = le.fit_transform(data["smoker"])
+print("\nSklearn Label Encoding for 'smoker':")
+print(dict(zip(le.classes_, le.transform(le.classes_))))
+print(data[["smoker"]].head(10))
 
 # One Hot Encoding: Encode the 'region' column
 # TODO: Create a one hot encoder instance and encode the 'region' column
+ohe = OneHotEncoder(sparse_output=False, drop="first")
+region_encoded = ohe.fit_transform(data[["region"]])
+region_columns = ohe.get_feature_names_out(["region"])
 
 # TODO: Convert the result into a DataFrame with appropriate column names
+region_df = pd.DataFrame(region_encoded, columns=region_columns)
+data = pd.concat([data.reset_index(drop=True), region_df.reset_index(drop=True)], axis=1)
+data.drop(columns=["region"], inplace=True)
+
+print("\nSklearn One Hot Encoding for 'region':")
+print(region_df.head(10))
+
+# Display the updated dataframe
+print("\nFinal Dataframe after encoding:")
+print(data.head(15))
